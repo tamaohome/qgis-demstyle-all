0281054f474668d749c1846499b2b1e69b223731
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-02-24 16:14:02 +0900

    ♻️ refactor(ui): オーバーライドメソッドを明示化

--- ui/mouse_release_map_tool.py
+++ ui/mouse_release_map_tool.py
@@ -1,17 +1,20 @@
+from typing import override
 from qgis.gui import QgsMapMouseEvent, QgsMapToolEmitPoint
 
 
 class MouseReleaseMapTool(QgsMapToolEmitPoint):
     """マウスリリースにより発火するマップツール"""
 
-    def canvasPressEvent(self, event: QgsMapMouseEvent):
+    @override
+    def canvasPressEvent(self, e: QgsMapMouseEvent):
         # マウス押下時の挙動を無効化
         pass
 
-    def canvasReleaseEvent(self, event: QgsMapMouseEvent):
+    @override
+    def canvasReleaseEvent(self, e: QgsMapMouseEvent):
         # マウスが離された位置の地図座標を取得
-        point = self.toMapCoordinates(event.pos())
+        point = self.toMapCoordinates(e.pos())
 
         # 本来クリック時に飛ぶはずの canvasClicked シグナルを
         # リリースのタイミングで手動で発信（emit）する
-        self.canvasClicked.emit(point, event.button())
+        self.canvasClicked.emit(point, e.button())
