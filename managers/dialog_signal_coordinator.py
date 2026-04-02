a3be393379c4566365e1cee4afa080887770d69c
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-04-02 13:42:48 +0900

    ♻️ refactor: signal/slot 配線処理を整理

--- managers/dialog_signal_coordinator.py
+++ managers/dialog_signal_coordinator.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 
+from collections.abc import Callable
 from typing import TYPE_CHECKING
+from typing import TypeAlias
 
 from PyQt5.QtCore import Qt
 
 if TYPE_CHECKING:
     from ..ui.demstyle_all_dialog import DEMStyleAllDialog
+
+VoidCallback: TypeAlias = Callable[[], None]
+IndexChangedCallback: TypeAlias = Callable[[int], None]
 
 
 class DialogSignalCoordinator:
@@ -15,25 +20,38 @@ class DialogSignalCoordinator:
         self.dialog = dialog
 
     def bind(self) -> None:
-        # 主操作の配線
+        self._bind_primary_controls()
+        self._bind_checkbox_persistence()
+
+    def _bind_primary_controls(self) -> None:
+        """操作系の signal/slot を配線する。"""
         self.dialog.dataRangeSlider.valueChanged.connect(
             self.dialog.dem_layer_range_manager.handle_slider_change
         )
-        self.dialog.setElevationButton.clicked.connect(self.dialog.start_capture_mode)
+
+        on_start_capture: VoidCallback = self.dialog.start_capture_mode
+        self.dialog.setElevationButton.clicked.connect(on_start_capture)
+
         self.dialog.elevation_inputs.connect_value_changed(
             self.dialog.elevation_manager.on_min_elevation_changed,
             self.dialog.elevation_manager.on_mid_elevation_changed,
             self.dialog.elevation_manager.on_max_elevation_changed,
         )
-        self.dialog.map_tool.canvasClicked.connect(self.dialog.dem_layer_range_manager.handle_get_elevation)
-        self.dialog.okButton.clicked.connect(self.dialog.on_ok_clicked)
-        self.dialog.cancelButton.clicked.connect(self.dialog.on_cancel_clicked)
-        self.dialog.searchStringRenameButton.clicked.connect(
-            self.dialog.on_search_string_rename_button_clicked
-        )
-        self.dialog.featureLayerComboBox.currentIndexChanged.connect(self.dialog.on_feature_layer_changed)
 
-        # チェックボックス状態を設定へ保存
+        self.dialog.map_tool.canvasClicked.connect(self.dialog.dem_layer_range_manager.handle_get_elevation)
+
+        on_ok_clicked: VoidCallback = self.dialog.on_ok_clicked
+        on_cancel_clicked: VoidCallback = self.dialog.on_cancel_clicked
+        on_rename_clicked: VoidCallback = self.dialog.on_search_string_rename_button_clicked
+        on_feature_layer_changed: IndexChangedCallback = self.dialog.on_feature_layer_changed
+
+        self.dialog.okButton.clicked.connect(on_ok_clicked)
+        self.dialog.cancelButton.clicked.connect(on_cancel_clicked)
+        self.dialog.searchStringRenameButton.clicked.connect(on_rename_clicked)
+        self.dialog.featureLayerComboBox.currentIndexChanged.connect(on_feature_layer_changed)
+
+    def _bind_checkbox_persistence(self) -> None:
+        """チェックボックス変更時に設定を保存する。"""
         self.dialog.enableAttrTableUpdateCheckBox.stateChanged.connect(
             lambda checked: self.dialog.settings.save_enable_attr_table_update(
                 checked == Qt.CheckState.Checked
